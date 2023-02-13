25a8591c8336f44d7dbc47f6d06d8669f0941f8c
unknown <awdcgy@hanyang.ac.kr>
2023-02-13 15:49:49 +0900

    first init

---
+++ src/main/java/com/sifter/mrcsearch/MrcSearchApplication.java
@@ -0,0 +1,13 @@
+package com.sifter.mrcsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrcSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrcSearchApplication.class, args);
+	}
+
+}
