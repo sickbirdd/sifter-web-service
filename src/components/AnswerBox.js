47f835a0c5bd4384980fc46149478eeeb6e80b79
HanSeo An <gkstj0604@hanyang.ac.kr>
2023-02-23 16:23:50 +0900

    Solve conflict 2

--- src/components/AnswerBox.js
+++ src/components/AnswerBox.js
@@ -1,16 +1,27 @@
 import {React} from 'react';
 
-function AnswerBox({text, rank, score}) {
+function AnswerBox({text, score, img, type}) {
     return (
-        <span>
-            <div className="rank">{`Top ${rank}`}</div>
+        <div className={`answerbox-${type}`}>
+            {
+                    (img === null) ? "" : <img alt="Medal" className="medal" src={img}/> 
+            }
             <div className="answer">
                 {
                     `ANSWER : ${text}`
                 }
             </div>
-        </span>
+            <div className="score">
+                {
+                    `${score.toFixed(5)}`
+                }
+            </div>
+        </div>
     )
 }
-
+AnswerBox.defaultProps = { 
+    img: null,
+    text: "정답 예시",
+    score: 0.0
+}
 export default AnswerBox;
