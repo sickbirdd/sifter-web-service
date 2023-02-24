18805ac5b3033bdc6248399ec751abdd592e0087
HanSeo An <gkstj0604@hanyang.ac.kr>
2023-02-24 10:18:40 +0900

    Upload example question sample

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,6 +7,7 @@ import BronzeMedal from '../img/bronze.png';
 
 function Content({data, isLoad, topAnswers, isClick, context, setContext}) {
     const [visible, setVisible] = useState(false);
+    const [exam, setExam] = useState(-1);
     return (
         <div className="main">
             {/* <!-- 
@@ -16,7 +17,17 @@ function Content({data, isLoad, topAnswers, isClick, context, setContext}) {
                 정답 -> result ... 검색 시 결과 띄워줘야 한다
             --> */}
             <div className={isLoad ? "hide" : "example"}>
-                예시 질문?
+                <div className='exampleList'>
+                    <button className='exampleBtn' onClick={() => exam !== 1 ? setExam(1) : setExam(-1)}>예시 질문 1</button> 
+                    <button className='exampleBtn' onClick={() => exam !== 2 ? setExam(2) : setExam(-1)}>예시 질문 2</button> 
+                    <button className='exampleBtn' onClick={() => exam !== 3 ? setExam(3) : setExam(-1)}>예시 질문 3</button> 
+                    <button className='exampleBtn' onClick={() => exam !== 4 ? setExam(4) : setExam(-1)}>예시 질문 4</button> 
+                    <button className='exampleBtn' onClick={() => exam !== 5 ? setExam(5) : setExam(-1)}>예시 질문 5</button>
+                </div>
+                <div className={exam === -1 ? "hide" : "exampleAnswer " + exam}>
+                    <div>예시 정답 {exam}</div>
+                    <div>예시 지문 {exam}</div>
+                </div>
             </div>
             <div className={isLoad ? "result" : "hide"}>
                 {   
