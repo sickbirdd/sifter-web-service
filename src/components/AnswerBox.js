afc65a6542c4a530bbd213c08da409a0541223da
NotOneWest <gkstj0604@gmail.com>
2023-05-13 19:13:26 +0900

    Modfiy front

--- src/components/AnswerBox.js
+++ src/components/AnswerBox.js
@@ -1,29 +1,22 @@
 import {React} from 'react';
 import trophy from '../img/trophy.png';
-function AnswerBox({text, score, img, type}) {
+function AnswerBox({text, img, type}) {
     return (
         <div className={`answerbox ${type}`}>
             <div className="ranking">
                 {
                         (img === "") ? <img alt="Medal" className="medal" src={trophy}/> : <img alt="Medal" className="medal" src={img}/> 
                 }
-                <div className="score">
-                    {
-                        `${score.toFixed(5)}`
-                    }
-                </div>
             </div>
             <div className="answer">
                 {
                     `ANSWER : ${text}`
                 }
             </div>
-            
         </div>
     )
 }
 AnswerBox.defaultProps = { 
-    text: "정답 예시",
-    score: 0.0
+    text: "정답 예시"
 }
 export default AnswerBox;
